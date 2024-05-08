b2acde23772d8df83fe17e6ef5a1a67ea69985a7
Aponia <brianmv@uci.edu>
2024-05-08 10:24:15 -0700

    feat(eden-query): ✨ treaty mvp done

--- packages/svelte/src/internal/infinite.ts
+++ packages/svelte/src/internal/infinite.ts
@@ -1,11 +1,15 @@
+import type { RouteSchema } from 'elysia'
+
 export type GetRequestInput<T extends { params?: any; query?: any }> = T
 
 export type InfiniteCursorKey = 'cursor'
 
 export type ReservedInfiniteQueryKeys = InfiniteCursorKey | 'direction'
 
-export type InfiniteInput<T extends Record<string, any>> =
-  InfiniteCursorKey extends keyof (T['params'] & T['query']) ? T : never
+export type InfiniteInput<T extends RouteSchema> = InfiniteCursorKey extends keyof (T['params'] &
+  T['query'])
+  ? T
+  : never
 
 export type InfiniteRoutes<T> = {
   [K in keyof T as T[K] extends {
@@ -14,3 +18,5 @@ export type InfiniteRoutes<T> = {
     ? K
     : never]: T[K]
 }
+
+export type N = InfiniteCursorKey extends keyof Record<never, string> | undefined ? true : false
--- packages/svelte/src/treaty/types.ts
+++ packages/svelte/src/treaty/types.ts
@@ -9,9 +9,11 @@ import type {
   StoreOrVal,
 } from '@tanstack/svelte-query'
 import { Elysia } from 'elysia'
-import type { RouteSchema, Prettify } from 'elysia/types'
+import type { MaybeArray, MaybePromise, Prettify, RouteSchema } from 'elysia/types'
+
 import type { HttpMutationMethod, HttpQueryMethod, HttpSubscriptionMethod } from '../internal/http'
-import type { InfiniteInput } from '../internal/infinite'
+import type { InferRouteError, InferRouteInput, InferRouteOutput } from '../internal/infer'
+import type { InfiniteCursorKey, ReservedInfiniteQueryKeys } from '../internal/infinite'
 import type { EdenQueryParams, EdenSubscribeParams } from '../internal/params'
 import type { Join, Stringable } from '../utils/join'
 
@@ -22,16 +24,6 @@ export interface TreatyBaseOptions {
   fetch?: RequestInit
 }
 
-const e = new Elysia()
-  .get('/i/:cursor', (context) => {
-    return true ? context.error('Locked') : 'hi'
-  })
-  .post('/a/c', () => 123)
-
-export type A = (typeof e)['_routes']['i'][':cursor']['get']['response']
-
-export type O = Treaty.Sign<(typeof e)['_routes']>['i'][':cursor']['get']
-
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace Treaty {
   export type Create<App extends Elysia<any, any, any, any, any, any, any, any>> = App extends {
@@ -40,19 +32,115 @@ export namespace Treaty {
     ? Prettify<Sign<Schema>>
     : 'Please install Elysia before using Eden'
 
-  export type Sign<in out Route extends Record<string, any>> = {
+  export type Sign<in out Route extends Record<string, any>, TPath extends any[] = []> = {
     [K in keyof Route]: K extends 'subscribe'
       ? EdenSubscribeParams<Route[K]>
       : Route[K] extends RouteSchema
-      ? Prettify<TreatyBaseOptions & EdenQueryParams<any, Route[K]>>
-      : Sign<Route[K]>
+      ? TreatyQueryHooks<Route[K], K, TPath>
+      : Sign<Route[K], [...TPath, K]>
   }
+
+  export interface Config {
+    fetch?: Omit<RequestInit, 'headers' | 'method'>
+    fetcher?: typeof fetch
+    headers?: MaybeArray<
+      | RequestInit['headers']
+      | ((path: string, options: RequestInit) => RequestInit['headers'] | void)
+    >
+    onRequest?: MaybeArray<(path: string, options: RequestInit) => MaybePromise<RequestInit | void>>
+    onResponse?: MaybeArray<(response: Response) => MaybePromise<unknown>>
+    keepDomain?: boolean
+  }
+}
+
+export type TreatyQueryHooks<
+  TRoute extends RouteSchema,
+  TMethod,
+  TPath extends any[] = [],
+> = TMethod extends HttpQueryMethod
+  ? TreatyCreateQuery<TRoute, TPath>
+  : TMethod extends HttpMutationMethod
+  ? TreatyCreateMutation<TRoute, TPath>
+  : TMethod extends HttpSubscriptionMethod
+  ? TreatyCreateSubscription<TRoute, TPath>
+  : never
+
+export type TreatyCreateQuery<
+  TRoute extends RouteSchema,
+  TPath extends any[] = [],
+  TParams extends EdenQueryParams<any, TRoute> = EdenQueryParams<any, TRoute>,
+  TInput = InferRouteInput<TRoute, ReservedInfiniteQueryKeys>,
+  TOutput = InferRouteOutput<TRoute>,
+  TError = InferRouteError<TRoute>,
+  TEndpoint = TreatyQueryKey<TPath>,
+> = {
+  createQuery: (
+    options: StoreOrVal<
+      TParams & {
+        queryOptions?: Omit<
+          CreateQueryOptions<TOutput, TError, TOutput, [TEndpoint, TInput]>,
+          'queryKey'
+        >
+      }
+    >,
+  ) => CreateQueryResult<TOutput, TError>
+} & (InfiniteCursorKey extends keyof (TParams['params'] & TParams['query'])
+  ? TreatyCreateInfiniteQuery<TRoute, TPath>
+  : {})
+
+export type TreatyCreateInfiniteQuery<
+  TRoute extends RouteSchema,
+  TPath extends any[] = [],
+  TParams extends EdenQueryParams<any, TRoute> = EdenQueryParams<any, TRoute>,
+  TInput = InferRouteInput<TRoute>,
+  TOutput = InferRouteOutput<TRoute>,
+  TError = InferRouteError<TRoute>,
+  TEndpoint = TreatyQueryKey<TPath>,
+> = {
+  createInfiniteQuery: (
+    options: StoreOrVal<
+      TParams & {
+        queryOptions: Omit<
+          CreateInfiniteQueryOptions<TOutput, TError, TOutput, [TEndpoint, TInput]>,
+          'queryKey'
+        >
+      }
+    >,
+  ) => CreateInfiniteQueryResult<InfiniteData<TOutput>, TError>
+}
+
+export type TreatyCreateMutation<
+  TRoute extends RouteSchema,
+  _TPath extends any[] = [],
+  TInput = EdenQueryParams<any, TRoute>,
+  TOutput = InferRouteOutput<TRoute>,
+  TError = InferRouteError<TRoute>,
+  /**
+   * TODO: what is TContext for a fetch request mutation?
+   */
+  TContext = unknown,
+> = {
+  createMutation: (
+    options?: CreateMutationOptions<TOutput, TError, TInput, TContext>,
+  ) => CreateMutationResult<TOutput, TError, TInput, TContext>
+}
+
+/**
+ * TODO
+ */
+export type TreatyCreateSubscription<
+  TRoute extends RouteSchema,
+  TPath extends any[] = [],
+  TParams extends EdenQueryParams<any, TRoute> = EdenQueryParams<any, TRoute>,
+> = {
+  options: Prettify<TreatyBaseOptions & TParams>
+  queryKey: TreatyQueryKey<TPath>
 }
 
 /**
  * Calculates the query key.
  */
-export type TreatyQueryKey<T extends Stringable[] = []> = Join<T, '/', '/'>
+export type TreatyQueryKey<T extends Stringable[] = []> = Join<T, '/'>
 
 /**
  */
@@ -83,71 +171,3 @@ export type TreatyError<TResponse extends Record<number, unknown>> = {
   status: number
   headers: RequestInit['headers']
 }
-
-// /**
-//  * @type {TFetchFn} the function.
-//  * @type {TMethod} the actual method.
-//  */
-// export type MapHook<TFetchFn, TMethod> = TFetchFn extends ((
-//   ...args: [infer BodyOrOptions, infer OptionsOrUndefined]
-// ) => infer Response extends Record<number, unknown>)
-//   ? TMethod extends HttpQueryMethod
-//     ? {
-//         createQuery: (
-//           options: StoreOrVal<
-//             BodyOrOptions & {
-//               queryOptions?: Omit<
-//                 CreateQueryOptions<
-//                   TreatyData<Response>,
-//                   TreatyError<Response>,
-//                   TreatyData<Response>,
-//                   [TMethod /* TODO calculate query key? */, BodyOrOptions]
-//                 >,
-//                 'queryKey'
-//               >
-//             }
-//           >,
-//         ) => CreateQueryResult<TreatyData<Response>, TreatyError<Response>>
-//       } & (BodyOrOptions extends InfiniteInput<infer _FetchOptions>
-//         ? {
-//             createInfiniteQuery: (
-//               options: StoreOrVal<
-//                 BodyOrOptions & {
-//                   queryOptions: Omit<
-//                     CreateInfiniteQueryOptions<
-//                       TreatyData<Response>,
-//                       TreatyError<Response>,
-//                       TreatyData<Response>,
-//                       [TMethod /* TODO calculate query key? */, BodyOrOptions]
-//                     >,
-//                     'queryKey'
-//                   >
-//                 }
-//               >,
-//             ) => CreateInfiniteQueryResult<
-//               InfiniteData<TreatyData<Response>>,
-//               TreatyError<Response>
-//             >
-//           }
-//         : never)
-//     : TMethod extends HttpMutationMethod
-//     ? {
-//         createMutation: (
-//           options?: CreateMutationOptions<
-//             TreatyData<Response>,
-//             TreatyError<Response>,
-//             BodyOrOptions,
-//             any
-//           >,
-//         ) => CreateMutationResult<TreatyData<Response>, TreatyError<Response>, BodyOrOptions, any>
-//       }
-//     : TMethod extends HttpSubscriptionMethod
-//     ? {
-//         body: BodyOrOptions
-//         options: OptionsOrUndefined
-//         output: Response
-//         createSubscription: TFetchFn
-//       }
-//     : never
-//   : never
-//
