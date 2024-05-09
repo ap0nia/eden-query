3f53eb33e0b87d5bdcefbdc3bf39ae88b7713c9c
Aponia <brianmv@uci.edu>
2024-05-09 10:59:04 -0700

    refactor(eden query): hooks

--- packages/svelte/src/fetch/hooks.ts
+++ packages/svelte/src/fetch/hooks.ts
@@ -1,21 +1,29 @@
-import type {
-  CreateBaseMutationResult,
-  CreateInfiniteQueryOptions,
-  CreateInfiniteQueryResult,
-  CreateMutationOptions,
-  CreateQueryOptions,
-  CreateQueryResult,
-  InfiniteData,
-  MutateOptions,
-  StoreOrVal,
+import type { EdenFetch } from '@elysiajs/eden/fetch'
+import {
+  type CreateBaseMutationResult,
+  createInfiniteQuery,
+  type CreateInfiniteQueryOptions,
+  type CreateInfiniteQueryResult,
+  createMutation,
+  type CreateMutationOptions,
+  createQuery,
+  type CreateQueryOptions,
+  type CreateQueryResult,
+  type InfiniteData,
+  type MutateOptions,
+  type StoreOrVal,
 } from '@tanstack/svelte-query'
-import type { Readable } from 'svelte/store'
+import type { Elysia } from 'elysia'
+import { get, type Readable, writable } from 'svelte/store'
 
 import type { HttpMutationMethod, HttpQueryMethod } from '../internal/http'
 import type { InferRouteError, InferRouteInput, InferRouteOutput } from '../internal/infer'
 import type { InfiniteRoutes, ReservedInfiniteQueryKeys } from '../internal/infinite'
-import type { EdenRequestOptions } from '../internal/options'
+import type { EdenRequestOptions, SvelteQueryProxyConfig } from '../internal/options'
+import { getQueryKey } from '../internal/query'
+import type { TreatyToPath } from '../internal/treaty-to-path'
 import type { Filter } from '../utils/filter'
+import { isStore } from '../utils/is-store'
 import type { Override } from '../utils/override'
 
 export type EdenFetchQueryHooks<TSchema extends Record<string, any>> = {
@@ -121,3 +129,197 @@ export type EdenFetchMutationFunction<
     TContext
   >,
 ) => void
+
+/**
+ * Create the hooks.
+ */
+export function createHooks<T extends Elysia<any, any, any, any, any, any, any, any>>(
+  fetch: EdenFetch.Fn<
+    // @ts-expect-error Type 'unknown' is not assignable to type 'Record<string, any>'
+    TreatyToPath<T['_routes']>
+  >,
+  config?: SvelteQueryProxyConfig,
+) {
+  return {
+    createQuery: (
+      endpoint: string,
+      options: StoreOrVal<EdenRequestOptions & { queryOptions?: Partial<CreateQueryOptions> }>,
+    ) => {
+      const optionsValue = isStore(options) ? get(options) : options
+
+      const abortOnUnmount =
+        Boolean(config?.abortOnUnmount) || Boolean(optionsValue.eden?.abortOnUnmount)
+
+      const { queryOptions, ...rest } = optionsValue
+
+      const baseQueryOptions = {
+        queryKey: getQueryKey(endpoint, optionsValue, 'query'),
+        queryFn: async (context) => {
+          return await fetch(
+            endpoint as any,
+            {
+              ...rest,
+              signal: abortOnUnmount ? context.signal : undefined,
+            } as EdenRequestOptions,
+          )
+        },
+        ...queryOptions,
+      } satisfies CreateQueryOptions
+
+      if (!isStore(options)) {
+        return createQuery(baseQueryOptions as any)
+      }
+
+      const optionsStore = writable(baseQueryOptions, (set) => {
+        const unsubscribe = options.subscribe((newInput) => {
+          const { queryOptions, ...rest } = optionsValue
+
+          set({
+            ...baseQueryOptions,
+            queryKey: getQueryKey(endpoint, newInput, 'query'),
+            queryFn: async (context) => {
+              return await fetch(
+                endpoint as any,
+                {
+                  ...rest,
+                  signal: abortOnUnmount ? context.signal : undefined,
+                } as EdenRequestOptions,
+              )
+            },
+            ...queryOptions,
+          })
+        })
+
+        return unsubscribe
+      })
+
+      return createQuery(optionsStore as any)
+    },
+    createInfiniteQuery: (
+      endpoint: string,
+      options: StoreOrVal<
+        EdenRequestOptions & { queryOptions?: Partial<CreateInfiniteQueryOptions> }
+      >,
+    ) => {
+      const optionsValue = isStore(options) ? get(options) : options
+
+      const abortOnUnmount =
+        Boolean(config?.abortOnUnmount) || Boolean(optionsValue.eden?.abortOnUnmount)
+
+      const { queryOptions, ...rest } = optionsValue
+
+      const baseQueryOptions = {
+        queryKey: getQueryKey(endpoint, optionsValue, 'infinite'),
+        queryFn: async (context) => {
+          // FIXME: scuffed way to set cursor.
+          if (rest.query) {
+            rest.query['cursor'] = context.pageParam
+          }
+
+          if (rest.params) {
+            rest.params['cursor'] = context.pageParam
+          }
+
+          return await fetch(
+            endpoint as any,
+            {
+              ...rest,
+              signal: abortOnUnmount ? context.signal : undefined,
+            } as EdenRequestOptions,
+          )
+        },
+        ...queryOptions,
+      } as CreateInfiniteQueryOptions
+
+      if (!isStore(options)) {
+        return createInfiniteQuery(baseQueryOptions)
+      }
+
+      const optionsStore = writable(baseQueryOptions, (set) => {
+        const unsubscribe = options.subscribe((newInput) => {
+          const { queryOptions, ...rest } = newInput
+
+          set({
+            ...baseQueryOptions,
+            queryKey: getQueryKey(endpoint, newInput, 'infinite'),
+            queryFn: async (context) => {
+              // FIXME: scuffed way to set cursor.
+              if (rest.query) {
+                rest.query['cursor'] = context.pageParam
+              }
+
+              if (rest.params) {
+                rest.params['cursor'] = context.pageParam
+              }
+
+              return await fetch(
+                endpoint as any,
+                {
+                  ...rest,
+                  signal: abortOnUnmount ? context.signal : undefined,
+                } as EdenRequestOptions,
+              )
+            },
+            ...queryOptions,
+          })
+        })
+
+        return unsubscribe
+      })
+
+      return createInfiniteQuery(optionsStore)
+    },
+    createMutation: (endpoint: string, options?: CreateMutationOptions) => {
+      const optionsValue = isStore(options) ? get(options) : options
+
+      const baseOptions = {
+        mutationKey: [endpoint],
+        mutationFn: async (variables: any) => {
+          return await fetch(endpoint as any, variables)
+        },
+        onSuccess(data, variables, context) {
+          const originalFn = () => optionsValue?.onSuccess?.(data, variables, context)
+
+          return config?.overrides?.createMutation?.onSuccess != null
+            ? config.overrides.createMutation.onSuccess({
+                meta: optionsValue?.meta as any,
+                originalFn,
+              })
+            : originalFn()
+        },
+        ...optionsValue,
+      } satisfies CreateMutationOptions
+
+      if (!isStore(options)) {
+        return createMutation(baseOptions)
+      }
+
+      const optionsStore = writable(baseOptions, (set) => {
+        const unsubscribe = options.subscribe((newInput) => {
+          set({
+            ...baseOptions,
+            mutationKey: [endpoint],
+            mutationFn: async (variables: any) => {
+              return await fetch(endpoint as any, variables)
+            },
+            onSuccess(data, variables, context) {
+              const originalFn = () => newInput?.onSuccess?.(data, variables, context)
+
+              return config?.overrides?.createMutation?.onSuccess != null
+                ? config.overrides.createMutation.onSuccess({
+                    meta: newInput?.meta as any,
+                    originalFn,
+                  })
+                : originalFn()
+            },
+            ...newInput,
+          })
+        })
+
+        return unsubscribe
+      })
+
+      return createMutation(optionsStore)
+    },
+  }
+}
--- packages/svelte/src/fetch/index.ts
+++ packages/svelte/src/fetch/index.ts
@@ -1,31 +1,54 @@
 import { edenFetch } from '@elysiajs/eden'
 import type { EdenFetch } from '@elysiajs/eden/fetch'
-import {
-  createInfiniteQuery,
-  type CreateInfiniteQueryOptions,
-  createMutation,
-  type CreateMutationOptions,
-  createQuery,
-  type CreateQueryOptions,
-  QueryClient,
-  type StoreOrVal,
-} from '@tanstack/svelte-query'
+import { QueryClient } from '@tanstack/svelte-query'
 import { Elysia } from 'elysia'
 import { getContext, setContext } from 'svelte'
-import { get, writable } from 'svelte/store'
 
-import type { HttpQueryMethod } from '../internal/http'
-import type { InferRouteInput, InferRouteOutput } from '../internal/infer'
-import type { EdenRequestOptions, SvelteQueryProxyConfig } from '../internal/options'
-import { getQueryKey } from '../internal/query'
+import type { SvelteQueryProxyConfig } from '../internal/options'
 import type { TreatyToPath } from '../internal/treaty-to-path'
-import type { Filter } from '../utils/filter'
 import type { IsOptional } from '../utils/is-optional'
-import { isStore } from '../utils/is-store'
 import { createContext, EDEN_CONTEXT_KEY, type EdenFetchQueryContext } from './context'
-import type { EdenFetchQueryHooks } from './hooks'
+import { createHooks, type EdenFetchQueryHooks } from './hooks'
 
 export type EdenFetchQueryConfig = EdenFetch.Config & SvelteQueryProxyConfig
+
+export type EdenFetchQuery<
+  TSchema extends Record<string, any>,
+  TConfig extends EdenFetchQueryConfig = EdenFetchQueryConfig,
+> = EdenFetchQueryHooks<TSchema> &
+  (IsOptional<TConfig, 'queryClient'> extends true
+    ? {
+        context?: EdenFetchQueryContext<TSchema>
+      }
+    : {
+        context: EdenFetchQueryContext<TSchema>
+      }) & {
+    /**
+     * Builder utility to strongly define the config in a second step.
+     */
+    config: <TNewConfig extends EdenFetchQueryConfig>(
+      newConfig: TNewConfig,
+    ) => EdenFetchQuery<TSchema, TNewConfig>
+
+    /**
+     * Official, initialized {@link edenFetch} instance.
+     */
+    fetch: EdenFetch.Fn<TSchema>
+
+    /**
+     */
+    setContext: (queryClient: QueryClient, configOverride?: EdenFetchQueryConfig) => void
+
+    /**
+     * Get the utilities from setContext.
+     */
+    getContext: () => EdenFetchQueryContext<TSchema>
+
+    /**
+     * Alias for {@link EdenFetchQuery.createContext}
+     */
+    useUtils: () => EdenFetchQueryContext<TSchema>
+  }
 
 /**
  * TODO: allow passing in an instance of {@link Elysia} for server-side usage.
@@ -65,223 +88,6 @@ export function createEdenFetchQuery<
     },
     getContext: getContextThunk,
     useUtils: getContextThunk,
-    createQuery: (
-      endpoint: string,
-      options: StoreOrVal<EdenRequestOptions & { queryOptions?: Partial<CreateQueryOptions> }>,
-    ) => {
-      const optionsValue = isStore(options) ? get(options) : options
-
-      const abortOnUnmount =
-        Boolean(config?.abortOnUnmount) || Boolean(optionsValue.eden?.abortOnUnmount)
-
-      const { queryOptions, ...rest } = optionsValue
-
-      const baseQueryOptions = {
-        queryKey: getQueryKey(endpoint, optionsValue, 'query'),
-        queryFn: async (context) => {
-          return await fetch(endpoint, {
-            ...rest,
-            signal: abortOnUnmount ? context.signal : undefined,
-          } as EdenRequestOptions)
-        },
-        ...queryOptions,
-      } satisfies CreateQueryOptions
-
-      if (!isStore(options)) {
-        return createQuery(baseQueryOptions as any)
-      }
-
-      const optionsStore = writable(baseQueryOptions, (set) => {
-        const unsubscribe = options.subscribe((newInput) => {
-          const { queryOptions, ...rest } = optionsValue
-
-          set({
-            ...baseQueryOptions,
-            queryKey: getQueryKey(endpoint, newInput, 'query'),
-            queryFn: async (context) => {
-              return await fetch(endpoint, {
-                ...rest,
-                signal: abortOnUnmount ? context.signal : undefined,
-              } as EdenRequestOptions)
-            },
-            ...queryOptions,
-          })
-        })
-
-        return unsubscribe
-      })
-
-      return createQuery(optionsStore as any)
-    },
-    createInfiniteQuery: (
-      endpoint: string,
-      options: StoreOrVal<
-        EdenRequestOptions & { queryOptions?: Partial<CreateInfiniteQueryOptions> }
-      >,
-    ) => {
-      const optionsValue = isStore(options) ? get(options) : options
-
-      const abortOnUnmount =
-        Boolean(config?.abortOnUnmount) || Boolean(optionsValue.eden?.abortOnUnmount)
-
-      const { queryOptions, ...rest } = optionsValue
-
-      const baseQueryOptions = {
-        queryKey: getQueryKey(endpoint, optionsValue, 'infinite'),
-        queryFn: async (context) => {
-          // FIXME: scuffed way to set cursor.
-          if (rest.query) {
-            rest.query['cursor'] = context.pageParam
-          }
-
-          if (rest.params) {
-            rest.params['cursor'] = context.pageParam
-          }
-
-          return await fetch(endpoint, {
-            ...rest,
-            signal: abortOnUnmount ? context.signal : undefined,
-          } as EdenRequestOptions)
-        },
-        ...queryOptions,
-      } as CreateInfiniteQueryOptions
-
-      if (!isStore(options)) {
-        return createInfiniteQuery(baseQueryOptions)
-      }
-
-      const optionsStore = writable(baseQueryOptions, (set) => {
-        const unsubscribe = options.subscribe((newInput) => {
-          const { queryOptions, ...rest } = newInput
-
-          set({
-            ...baseQueryOptions,
-            queryKey: getQueryKey(endpoint, newInput, 'infinite'),
-            queryFn: async (context) => {
-              // FIXME: scuffed way to set cursor.
-              if (rest.query) {
-                rest.query['cursor'] = context.pageParam
-              }
-
-              if (rest.params) {
-                rest.params['cursor'] = context.pageParam
-              }
-
-              return await fetch(endpoint, {
-                ...rest,
-                signal: abortOnUnmount ? context.signal : undefined,
-              } as EdenRequestOptions)
-            },
-            ...queryOptions,
-          })
-        })
-
-        return unsubscribe
-      })
-
-      return createInfiniteQuery(optionsStore)
-    },
-    createMutation: (endpoint: string, options?: CreateMutationOptions) => {
-      const optionsValue = isStore(options) ? get(options) : options
-
-      const baseOptions = {
-        mutationKey: [endpoint],
-        mutationFn: async (variables) => {
-          return await fetch(endpoint, variables)
-        },
-        onSuccess(data, variables, context) {
-          const originalFn = () => optionsValue?.onSuccess?.(data, variables, context)
-
-          return config?.overrides?.createMutation?.onSuccess != null
-            ? config.overrides.createMutation.onSuccess({
-                meta: optionsValue?.meta as any,
-                originalFn,
-              })
-            : originalFn()
-        },
-        ...optionsValue,
-      } satisfies CreateMutationOptions
-
-      if (!isStore(options)) {
-        return createMutation(baseOptions)
-      }
-
-      const optionsStore = writable(baseOptions, (set) => {
-        const unsubscribe = options.subscribe((newInput) => {
-          set({
-            ...baseOptions,
-            mutationKey: [endpoint],
-            mutationFn: async (variables) => {
-              return await fetch(endpoint, variables)
-            },
-            onSuccess(data, variables, context) {
-              const originalFn = () => newInput?.onSuccess?.(data, variables, context)
-
-              return config?.overrides?.createMutation?.onSuccess != null
-                ? config.overrides.createMutation.onSuccess({
-                    meta: newInput?.meta as any,
-                    originalFn,
-                  })
-                : originalFn()
-            },
-            ...newInput,
-          })
-        })
-
-        return unsubscribe
-      })
-
-      return createMutation(optionsStore)
-    },
+    ...createHooks(fetch, config),
   } as any
 }
-
-export type EdenFetchQuery<
-  TSchema extends Record<string, any>,
-  TConfig extends EdenFetchQueryConfig = EdenFetchQueryConfig,
-> = {
-  /**
-   * Official, initialized {@link edenFetch} instance.
-   */
-  fetch: EdenFetch.Fn<TSchema>
-
-  /**
-   * Get the utilities from setContext.
-   */
-  getContext: () => EdenFetchQueryContext<TSchema>
-
-  /**
-   * Alias for {@link EdenFetchQuery.createContext}
-   */
-  useUtils: () => EdenFetchQueryContext<TSchema>
-
-  /**
-   * Builder utility to strongly define the config in a second step.
-   */
-  config: <TNewConfig extends EdenFetchQueryConfig>(
-    newConfig: TNewConfig,
-  ) => EdenFetchQuery<TSchema, TNewConfig>
-} & EdenFetchQueryHooks<TSchema> &
-  (IsOptional<TConfig, 'queryClient'> extends true
-    ? {
-        context?: EdenFetchQueryContext<TSchema>
-      }
-    : {
-        context: EdenFetchQueryContext<TSchema>
-      })
-
-export type InferEdenQueryInput<
-  T extends Elysia<any, any, any, any, any, any, any, any>,
-  TEndpoint extends keyof Filter<T['_routes'], HttpQueryMethod>,
-  TMethod extends Uppercase<Extract<keyof T['_routes'][TEndpoint], HttpQueryMethod>>,
-  TRoute extends
-    T['_routes'][TEndpoint][Lowercase<TMethod>] = T['_routes'][TEndpoint][Lowercase<TMethod>],
-> = InferRouteInput<TRoute>
-
-export type InferEdenQueryOutput<
-  T extends Elysia<any, any, any, any, any, any, any, any>,
-  TEndpoint extends keyof Filter<T['_routes'], HttpQueryMethod>,
-  TMethod extends Uppercase<Extract<keyof T['_routes'][TEndpoint], HttpQueryMethod>>,
-  TRoute extends
-    T['_routes'][TEndpoint][Lowercase<TMethod>] = T['_routes'][TEndpoint][Lowercase<TMethod>],
-> = InferRouteOutput<TRoute>
