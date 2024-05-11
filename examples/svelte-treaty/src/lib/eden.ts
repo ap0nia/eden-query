e90c15b2e0cbe7848cd9a3449362dc045bfdcc36
Aponia <brianmv@uci.edu>
2024-05-11 16:54:08 -0700

    feat(eden query): infer

--- examples/svelte-treaty/src/lib/eden.ts
+++ examples/svelte-treaty/src/lib/eden.ts
@@ -1,5 +1,15 @@
-import { createEdenTreatyQuery } from '@ap0nia/eden-svelte-query'
+import { createEdenTreatyQuery, type TreatyCreateQuery } from '@ap0nia/eden-svelte-query'
 
 import type { App } from '$lib/server'
 
 export const eden = createEdenTreatyQuery<App>()
+
+/**
+ * Example for inferring.
+ */
+export type Infer = (typeof eden)['api']['wait']['get']['createQuery'] extends TreatyCreateQuery<
+  infer TRoute,
+  infer TPath
+>
+  ? { a: TRoute; b: TPath }
+  : never
