974ed913aec8d30584d150aba5ad5355603bee90
Aponia <brianmv@uci.edu>
2024-05-19 02:08:33 -0700

    chore: 🔧 remove extra files for now

--- packages/svelte/src/links/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './http-batch-link'
-export * from './http-link'
--- packages/svelte/src/utils/is-numeric-string.ts
+++
@@ -1,3 +0,0 @@
-export function isNumericString(message: string) {
-  return message.trim().length !== 0 && !Number.isNaN(Number(message))
-}
