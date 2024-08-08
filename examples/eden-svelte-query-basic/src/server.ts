5081a124fca8efbd500633c26ac5d06230ddd097
Aponia <brianmv@uci.edu>
2024-08-08 09:33:09 -0700

    chore(example-svelte): 🔧 explain order

--- examples/eden-svelte-query-basic/src/server.ts
+++ examples/eden-svelte-query-basic/src/server.ts
@@ -1,4 +1,4 @@
-import { batchPlugin } from '@elysiajs/eden-svelte-query'
+import { batchPlugin, transformPlugin } from '@elysiajs/eden-svelte-query'
 import { Elysia, t } from 'elysia'
 import SuperJSON from 'superjson'
 
@@ -95,34 +95,7 @@ const rootController = new Elysia()
   )
 
 export const app = new Elysia({ prefix: '/api' })
-  /**
-   * De-serialize incoming JSON data from the client using SuperJSON.
-   */
-  .onParse(async (context) => {
-    if (context.contentType !== 'application/json') return
-
-    const json = await context.request.json()
-
-    return await SuperJSON.deserialize(json)
-  })
-  /**
-   * Serialize outgoing JSON data from the server using SuperJSON.
-   */
-  .mapResponse((context) => {
-    // if (typeof context.response !== 'object') return
-
-    if (context.response instanceof Response) return
-
-    const serializedResponse = SuperJSON.serialize(context.response)
-
-    const text = JSON.stringify(serializedResponse)
-
-    return new Response(text, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-  })
+  .use(transformPlugin(SuperJSON))
   /**
    * Use the batch plugin after all transforms have been defined so it inherits them.
    */
