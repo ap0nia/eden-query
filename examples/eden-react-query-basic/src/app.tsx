6bd9478860004283e0f388249dda8ea75a2d3778
Aponia <brianmv@uci.edu>
2024-09-07 08:09:17 -0700

    feat(react): ✨ example with mutation and query params

--- examples/eden-react-query-basic/src/app.tsx
+++ examples/eden-react-query-basic/src/app.tsx
@@ -11,6 +11,7 @@ import BatchPage from './routes/batch/+page'
 import HelloPreloadPage, { load as helloPreloadLoader } from './routes/hello-preload/+page'
 import InfinitePage from './routes/infinite/+page'
 import MutationPage from './routes/mutation/+page'
+import MutationQueryParamPage from './routes/mutation-query-params/+page'
 import ReactiveInputPage from './routes/reactive-input/+page'
 import ReactiveParamsPage from './routes/reactive-params/+page'
 import UseQueriesPage from './routes/use-queries/+page'
@@ -77,6 +78,14 @@ export function App() {
                     <Link to="/reactive-input">reactive input box</Link>
                   </li>
                   <li>
+                    <Link to="/reactive-params">reactive params</Link>
+                  </li>
+                  <li>
+                    <Link to="/mutation-query-params">
+                      mutation with query and path parameter input
+                    </Link>
+                  </li>
+                  <li>
                     <Link to="/abort">abort</Link>
                   </li>
                   <li>
@@ -109,6 +118,10 @@ export function App() {
           {
             path: '/mutation',
             element: <MutationPage />,
+          },
+          {
+            path: '/mutation-query-params',
+            element: <MutationQueryParamPage />,
           },
           {
             path: '/abort',
