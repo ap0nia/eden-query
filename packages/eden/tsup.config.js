ef7456e0ad5c20b8e88bfbbae68a937154301bae
Aponia <brianmv@uci.edu>
2024-11-15 09:25:51 -0800

    feat: ✨ bundle packages with tsup

---
+++ packages/eden/tsup.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'tsup'
+
+const config = defineConfig({
+  entry: {
+    index: './src/index.ts',
+    client: './src/client.ts',
+    config: './src/config.ts',
+    constants: './src/constants.ts',
+    errors: './src/errors.ts',
+    http: './src/http.ts',
+    infer: './src/infer.ts',
+    request: './src/request.ts',
+    resolve: './src/resolve.ts',
+    server: './src/server.ts',
+    treaty: './src/treaty.ts',
+    ws: './src/ws.ts',
+  },
+  sourcemap: true,
+  format: ['esm', 'cjs'],
+})
+
+export default config
