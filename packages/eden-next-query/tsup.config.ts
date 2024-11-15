295132a0d6591bbabd604e6d7636e87f56389dba
Aponia <brianmv@uci.edu>
2024-11-15 09:30:27 -0800

    fix: 🐛 no dts for next build

--- packages/eden-next-query/tsup.config.ts
+++ packages/eden-next-query/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 const config = defineConfig({
   entry: ['./src/index.ts'],
   outDir: 'dist',
-  dts: true,
   format: ['esm'],
   external: ['react'],
   noExternal: ['@ap0nia/eden-react-query', '@ap0nia/eden'],
